bda1cf1a46aa1a853988d2a8fd421854f193bea6
Prayaas Jain <prayaasjain@gmail.com>
2013-08-27 16:51:42 -0700

    Commit #7 Update
    Modified search to handle NULL

--- pdfReaderWebSearch/ViewController.m
+++ pdfReaderWebSearch/ViewController.m
@@ -14,31 +14,57 @@
 
 @synthesize currentSearchEngine,searchTextField,selectSearchButton,highlighted,show;
 
+- (BOOL) shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    
+    if([identifier isEqualToString:@"Search"]) {
+    
+        if(searchTextField.text == NULL) {
+            
+            UIAlertView *nullSearchAlert = [[UIAlertView alloc] initWithTitle:@"Invalid Search" message:@"Search field cannot be empty" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil ];
+            [nullSearchAlert show];
+            return NO;
+        }
+        else {
+            return YES;
+        }
+    }
+}
+
 - (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if([segue.identifier isEqualToString:@"Search"]) {
-        
-        WebViewController *wvc = [segue destinationViewController];
-        [wvc setTitle:@"Search Results"];
-        wvc.selectedSearchEngine = selectSearchButton.titleLabel.text;
-        wvc.desiredSearch = searchTextField.text;
-        wvc.show = self.show;
-        wvc.vc = self;
-        
+    
+        if([segue.identifier isEqualToString:@"Search"]) {
+            
+                WebViewController *wvc = [segue destinationViewController];
+                [wvc setTitle:@"Search Results"];
+                wvc.selectedSearchEngine = selectSearchButton.titleLabel.text;
+                wvc.desiredSearch = searchTextField.text;
+                wvc.show = self.show;
+                wvc.vc = self;
+            
+        }
+        else if([segue.identifier isEqualToString:@"popOver"]) {
+
+            UIStoryboardPopoverSegue *popoverSegue;
+            popoverSegue = (UIStoryboardPopoverSegue *)segue;
+            
+            
+            UIPopoverController *popoverController;
+            popoverController = popoverSegue.popoverController;
+            popoverController.delegate = self;
+            
+            TableViewController *tableViewController = (TableViewController *)popoverSegue.destinationViewController;
+            tableViewController.popoverController = popoverController;
+            tableViewController.vc = self;
+        }
+    
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    if([alertView.title isEqual:@"Invalid Search"] && buttonIndex == 1) {
+        [self dismissViewControllerAnimated:YES completion:nil];
     }
-    else if([segue.identifier isEqualToString:@"popOver"])
-    {
-        UIStoryboardPopoverSegue *popoverSegue;
-        popoverSegue = (UIStoryboardPopoverSegue *)segue;
-        
-        
-        UIPopoverController *popoverController;
-        popoverController = popoverSegue.popoverController;
-        popoverController.delegate = self;
-        
-        TableViewController *tableViewController = (TableViewController *)popoverSegue.destinationViewController;
-        tableViewController.popoverController = popoverController;
-        tableViewController.vc = self;
-    }
+    
     
 }
 
