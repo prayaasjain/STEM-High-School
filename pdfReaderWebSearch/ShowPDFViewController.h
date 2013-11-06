6148a547b35402f61841962bd18b9544653cf31c
Prayaas Jain <prayaasjain@gmail.com>
2013-11-06 10:36:51 -0800

    Revert "changed pause menu action to pause music. changed pause button location."
    This reverts commit 230e9d9526e1f32c829c40469b9b5be3e026ef7e.

--- pdfReaderWebSearch/ShowPDFViewController.h
+++ pdfReaderWebSearch/ShowPDFViewController.h
@@ -6,20 +6,25 @@
 //  Copyright (c) 2013 Prayaas Jain. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+#import <PSPDFKit/PSPDFKit.h>
+
 #import "CustomView.h"
 #import "ViewController.h"
 #import "ListOfPDFViewController.h"
 
 @class ListOfPDFViewController;
 
-@interface ShowPDFViewController : PSPDFViewController <PSPDFViewControllerDelegate>
 
+@interface ShowPDFViewController : PSPDFViewController 
 @property (weak, nonatomic) IBOutlet UIWebView *pdfViewer;
 @property (weak, nonatomic) IBOutlet CustomView *backView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *searchButton;
 
--(void)loadPDF:(NSString *)path;
+-(void) loadPDF:(NSString *)path;
 -(void)loadURL:(NSURL *)url;
+-(void)addCustomButtons;
+- (IBAction)clickedSearchButton:(id)sender;
 
 @property (weak) NSString *selection;
 @property (weak) NSTimer  *timer;
